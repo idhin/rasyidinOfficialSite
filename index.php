158bb40db9a704cae9f7d3ff305ba89b9703db9e
Khulafaur Rasyidin <khulafaurrasyidin@Khulafaurs-MacBook-Pro.local>
2022-01-19 14:04:06 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<html> 
+    <head> 
+      <meta content='Little intro from Ahmad Malhadi (hexdemsion)' name='description'/>
+      <meta content='https://www.rasyid.in/' property='og:url'/>
+      <meta content='Tentang Khulafaur Rasyidin (Idin)' property='og:title'/>
+      <meta content='Hi, Idin was here.' property='og:description'/>
+      <link href='https://www.rasyid.in' rel='canonical'/>
+      <title>Rasyidin | About me.</title> 
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script> 
+      <link rel="stylesheet" type="text/css" href="index.css">
+    </head> 
+    <body> 
+      <script type="text/javascript" src="index.js"> 
+      </script> 
+      <div id="console"></div> 
+      
+      </script>
+    </body> 
+  </html> 
+  
